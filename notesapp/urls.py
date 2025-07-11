8242902c031c2f57e5ed3260c08579f27da71fbf
Ubuntu <ubuntu@ip-172-31-14-15.us-east-2.compute.internal>
2025-07-11 11:38:59 +0000

    Added HttpResponse for root URL

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', lambda request: HttpResponse("Django is running successfully!")),  #  updated route
+#    path('', TemplateView.as_view(template_name='index.html')),
 ]
